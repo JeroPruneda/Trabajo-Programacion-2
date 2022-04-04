5a4cc1caac1b828117905aecfb39127ec4e8b032
jsojo@udesa.edu.ar <jeronimosojo@MacBook-Air-de-Jero.local>
2022-04-04 17:05:03 -0300

    lista de zapatillas

--- Proyecto/data/db.js
+++ Proyecto/data/db.js
@@ -1,7 +1,64 @@
 const zapatilla = [
     {
-        
-
-    }
+        marca: "Nike",
+        modelo: "NIKE ZOOM 2K",
+        anio: 2016,
+        color: "black",
+    },
+    {
+        marca: "Nike",
+        modelo: "NIKE CRATER IMPACT",
+        anio: 2020,
+        color: "red",
+    },
+    {
+        marca: "Nike",
+        modelo: "NIKE REACT ART3MIS",
+        anio: 2015,
+        color: "white", 
+    },
+    {
+        marca: "Nike",
+        modelo: "NIKE AIR FORCE 1",
+        anio: 2016,
+        color: "white",
+    },
+    {
+        marca: "Nike",
+        modelo: "NIKE DUNK",
+        anio: 2000,
+        color: "gray",
+    },
+    {
+        marca: "Nike",
+        modelo: "NIKE AIR MORE UPTEMPO",
+        anio: 2019,
+        color: "black",
+    },
+    {
+        marca: "Adidas",
+        modelo: "ULTRABOOST",
+        anio: 2019,
+        color: "black",
+    },
+    {
+        marca: "Adidas",
+        modelo: "YEEZY",
+        anio: 2014,
+        color: "blue",
+    },
+    {
+        marca: "Adidas",
+        modelo: "NMD R1",
+        anio: 2010,
+        color: "white",
+    },
+    {
+        marca: "Converse",
+        modelo: "CHUCK TAYLOR ALL STAR",
+        anio: 2020,
+        color: "black",
+    },
+    
 ]
 module.exports = zapatilla
