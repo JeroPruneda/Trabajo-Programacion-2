2e247a07462aa3065d763d4e8f55eedee166eaa3
Jeronimo Sojo <jeronimosojo@MacBook-Air-de-Jero.local>
2022-04-04 19:48:36 -0300

    orden

--- Proyecto/data/db.js
+++ Proyecto/data/db.js
@@ -4,16 +4,16 @@ const data = {
     },
 
     productos: [
-        {marca: "Nike", modelo: "NIKE ZOOM 2K", anio: 2016, color: "black"}, 
-        {marca: "Nike", modelo: "NIKE CRATER IMPACT", anio: 2020, color: "red"},
-        {marca: "Nike", modelo: "NIKE REACT ART3MIS", anio: 2015,color: "white"},
-        {marca: "Nike", modelo: "NIKE AIR FORCE 1", anio: 2016, color: "white"},
-        {marca: "Nike", modelo: "NIKE DUNK", anio: 2000, color: "gray"},
-        {marca: "Nike", modelo: "NIKE AIR MORE UPTEMPO", anio: 2019, color: "black"},
-        {marca: "Adidas", modelo: "ULTRABOOST", anio: 2019, color: "black"},
-        {marca: "Adidas", modelo: "YEEZY", anio: 2014, color: "blue"},
-        {marca: "Adidas", modelo: "NMD R1", anio: 2010, color: "white"},
-        {marca: "Converse", modelo: "CHUCK TAYLOR ALL STAR", anio: 2020, color: "black"},
+        {marca: "Nike", modelo: "NIKE ZOOM 2K"}, 
+        {marca: "Nike", modelo: "NIKE CRATER IMPACT"},
+        {marca: "Nike", modelo: "NIKE REACT ART3MIS"},
+        {marca: "Nike", modelo: "NIKE AIR FORCE 1"},
+        {marca: "Nike", modelo: "NIKE DUNK"},
+        {marca: "Nike", modelo: "NIKE AIR MORE UPTEMPO"},
+        {marca: "Adidas", modelo: "ULTRABOOST"},
+        {marca: "Adidas", modelo: "YEEZY"},
+        {marca: "Adidas", modelo: "NMD R1"},
+        {marca: "Converse", modelo: "CHUCK TAYLOR ALL STAR" },
     ],
     
     comentarios: [
