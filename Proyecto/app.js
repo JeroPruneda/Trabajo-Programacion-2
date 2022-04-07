5d70872b822235e36a17886431d260db357cb8af
Jeronimo30 <jsojo@udesa.edu.ar>
2022-04-07 14:38:51 -0300

    borrar login y register

--- Proyecto/app.js
+++ Proyecto/app.js
@@ -7,9 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productoDetalleRouter = require('./routes/productoDetalle');
-var registerRouter = require("./routes/register");
 var profileRouter = require("./routes/profile");
-var loginRouter = require("./routes/login");
 
 
 var app = express();
@@ -27,8 +25,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/productoDetalle', productoDetalleRouter);
-app.use("/register", registerRouter);
-app.use("/login", loginRouter);
 app.use("/profile", profileRouter);
 
 
