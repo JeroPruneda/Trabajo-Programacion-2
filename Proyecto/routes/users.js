37adcf23ef7a938ad0a0041d2be7ef0e7e5a0bdf
JeroPruneda <89102212+JeroPruneda@users.noreply.github.com>
2022-05-09 16:21:13 -0300

    .

--- Proyecto/routes/users.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var controller = require('../controllers/userController')
-
-router.get('/me', controller.me);
-router.get('/me/edit', controller.edit);
-
-module.exports = router;
