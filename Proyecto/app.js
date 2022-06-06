00156a91ea807057c84bf3dcff55813618ea0f06
Jeronimo30 <jsojo@udesa.edu.ar>
2022-06-06 17:54:48 -0300

    mejoras

--- Proyecto/app.js
+++ Proyecto/app.js
@@ -7,7 +7,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const session = require('express-session');
-var db = require('./database/models/');
+var db = require('./database/models');
 
 var indexRouter = require('./routes/index');
 var productoDetalleRouter = require('./routes/productoDetalle');
