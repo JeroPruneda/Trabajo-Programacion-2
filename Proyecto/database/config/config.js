6d79238fbb558860229af417da0d5d564ccc7f41
Jeronimo30 <jsojo@udesa.edu.ar>
2022-06-09 14:39:58 -0300

    mejoras

--- Proyecto/database/config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports ={
-    "development": {
-      "username": "Jeronimo",
-      "password": "Biologia12345",
-      "database": "datazapa",
-      "host": "127.0.0.1",
-      "dialect": "mysql"
-    }
-  }
