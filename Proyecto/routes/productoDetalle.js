319f5958069237f9fa9dbdda0a4dedeb639e2231
JeroPruneda <89102212+JeroPruneda@users.noreply.github.com>
2022-06-13 15:59:03 -0300

    cambios

--- Proyecto/routes/productoDetalle.js
+++ Proyecto/routes/productoDetalle.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 var controller = require("../controllers/productoDetalleController")
 
-router.get('/', controller.detalle);
+
 router.get('/add', controller.add);
 router.get('/show', controller.show);
-
+router.get('/:id', controller.detalle);
 router.post('/add', controller.guardar);
 
 
