c2278b95e520fb06fbfa50eab586f466bef7628e
Jeronimo30 <jsojo@udesa.edu.ar>
2022-06-14 20:39:52 -0300

    profile edit

--- Proyecto/controllers/profileController.js
+++ Proyecto/controllers/profileController.js
@@ -1,24 +1,29 @@
-var db = require("../database/models")
+var db = require("../database/models");
+const usuario = db.Usuarios;
+const productos = db.zapas
 
 const controller = {
     myProfile: function(req, res) {
-        db.Usuarios.findByPk(req.session.usuario.id, { include: [ { association: 'productos' } ] })
+        res.render('profile', { usuario, productos });
+      /*   db.Usuarios.findByPk(req.session.usuario.id, { include: [ { association: 'productos' } ] })
             .then(function (usuario) {
                 
                 res.render('profile', { usuario : usuario });
             })
             .catch(function (error) {
                 res.send(error)
-            });
+            }); */
     },
+    
     profile: function(req, res) {
-        db.Usuarios.findByPk(req.params.id, { include: [ { association: 'productos' } ] })
+        res.render('profile', { usuario, productos });
+        /* db.Usuarios.findByPk(req.params.id, { include: [ { association: 'productos' } ] })
             .then(function (usuario) {
                 res.render('profile', { usuario });
             })
             .catch(function (error) {
                 res.send(error)
-            });
+            }); */
     },
     edit: function(req, res) {
         res.render('index', { title: 'Edit'});
