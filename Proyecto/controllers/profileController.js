ff021543fcbd1022adfd547cc60ffffed7a32aea
JeroPruneda <89102212+JeroPruneda@users.noreply.github.com>
2022-06-19 15:20:01 -0300

    cambios

--- Proyecto/controllers/profileController.js
+++ Proyecto/controllers/profileController.js
@@ -1,6 +1,10 @@
 var db = require("../database/models");
 const usuario = db.Usuarios
 const productos = db.zapas
+var multer = require('multer');
+const upload = multer({
+    dest: '..public/images/upload'
+})
 
 const controller = {
      myProfile: function(req, res) {
@@ -42,9 +46,9 @@ const controller = {
                 res.send(error)
             });    */
     },
-    edit:  function(req, res) {
+    /* edit:  function(req, res) {
         res.render('profile-edit', { title: 'Edit'});
-    },
+    }, */
     /* function(req, res) {
          productos.findByPk(req.params.id)
              .then(function (usuario) {
@@ -54,8 +58,8 @@ const controller = {
                  res.send(error);
              })  */
      
-    update: function(req, res) {
-        /* if (req.file) req.body.perfil = (req.file.path).replace('public', ''); */
+   /*  update: function(req, res) {
+         if (req.file) req.body.perfil = (req.file.path).replace('public', ''); 
         db.Usuarios.update(req.body, { where: { id: req.params.id } })
             .then(function() {
                 res.redirect('/')
@@ -63,8 +67,29 @@ const controller = {
             .catch(function(error) {
                 res.send(error);
             })
-    }
-  
+    } */
+   
+          edit: function(req, res) {
+           db.Usuarios.findByPk(req.params.id)
+               .then(function (usuarios) {
+                   res.render('profile-edit', { usuarios });
+               })
+               .catch(function (error) {
+                   res.send(error);
+               })
+               },
+   
+           update: function(req, res) {
+           if (req.file) req.body.perfil = (req.file.path).replace('public', '');
+           db.Usuarios.update(req.body, { where: { id: req.params.id } })
+               .then(function(usuarios) {
+                   console.log(usuarios);
+                   res.redirect('/')
+               })
+               .catch(function(error) {
+                   res.send(error);
+               })
+       },
 
 }
 
