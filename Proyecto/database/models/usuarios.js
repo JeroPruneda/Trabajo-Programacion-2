fddaeb83f2c8e0186a39b9929ca68dac5daebe93
josesoutou <josesoutou@gmail.com>
2022-06-19 16:13:04 -0300

    cambios

--- Proyecto/database/models/usuarios.js
+++ Proyecto/database/models/usuarios.js
@@ -11,7 +11,6 @@ module.exports = function (sequelize, dataTypes) {
         fecha_de_nacimiento: { type: dataTypes.DATE},
         contrasenia: { type: dataTypes.STRING },
         perfil: { type: dataTypes.STRING },
-<<<<<<< HEAD
     
         // /* created_at : {
         //     type: dataTypes.DATE,
@@ -22,17 +21,6 @@ module.exports = function (sequelize, dataTypes) {
         //     allowNull: true,
         // }
         //  */
-=======
-        created_at : {
-            type: dataTypes.DATE,
-            allowNull:true,
-        },
-        updated_at: {
-            type: dataTypes.DATE,
-            allowNull: true,
-        } 
-         
->>>>>>> 4e47d1f0e66043f520c070db987510952d56839f
         
     }
     const configs = {
