f11c5fcb51c38b7b5c2225105eb4ebd66f686b3b
Jeronimo30 <jsojo@udesa.edu.ar>
2022-06-20 20:38:40 -0300

    mejroas

--- Proyecto/database/config/config.js
+++ Proyecto/database/config/config.js
@@ -2,7 +2,6 @@ module.exports = {
   "development": {
     "username": "root",
     "password": "Jeronimo30",
-    "password": "Malucarrara1",
     "database": "datazapa",
     "host": "127.0.0.1",
     "dialect": "mysql",
