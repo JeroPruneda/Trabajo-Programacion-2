8d6b5cb4e5027a1613318a577d1648447f98e4b4
josesoutou <josesoutou@gmail.com>
2022-06-22 10:00:08 -0300

    cambios

--- Proyecto/routes/productoDetalle.js
+++ Proyecto/routes/productoDetalle.js
@@ -1,20 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var controller = require("../controllers/productoDetalleController");
-
 var multer= require('multer');
-let path = require('path');
+const upload = multer({dest: 'public/images/uploads'});
 
-let storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, '../public/images/upload')) 
-    },
-    filename : function (req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
-    }
-})
-
-let upload = multer({storage : storage})
 
 /* 
 router.get('/', controller.index); */
@@ -24,7 +13,7 @@ router.post('/add', upload.single('imagenes'), controller.guardar);
 
 router.get('/:id', controller.detalle);
 router.post('/:id/borrar', controller.borrar);
-router.post('/:id/comment', upload.single("perfil"), controller.comment);
+router.post('/:id/', controller.comment);
 
 router.get('/:id/edit', controller.edit);
 router.post('/:id/edit', upload.single('imagenes'), controller.update);
